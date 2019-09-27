d7ae5b3793b8b97717da801cdc153a9a8359d13c
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-09-27 20:19:04 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by mikako kinugawa on 2019/09/27.
+//  Copyright © 2019 mikako.kinugawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
