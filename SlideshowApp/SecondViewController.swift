a19f01e03c8dbf5c9f9db337bde99e09f95970a8
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-09-27 20:36:28 +0900

    画面を2つ作成。 SecondViewController.swiftファイルを作成。 SecondViewControllerクラスを2つ目の画面に紐づけた。

---
+++ SlideshowApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  SlideshowApp
+//
+//  Created by mikako kinugawa on 2019/09/27.
+//  Copyright © 2019 mikako.kinugawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
