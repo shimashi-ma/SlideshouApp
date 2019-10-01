bb67119d87531fd66170318794469b19b4cd4e2a
mikako.kinugawa <kinugawa0807@yahoo.co.jp>
2019-10-01 14:01:19 +0900

    再生/停止ボタンで画像を自動送りできるようにした。 自動送り中は、進むボタン・戻るボタンを押せないようにした。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     //スライドショーの画像
     @IBOutlet weak var slideimage: UIImageView!
     
-    
     //画像のインデックス番号用の変数を用意
     var imageIndex = 0
 
-    
     //3枚の画像を定数imagesに入れておく
     let images = [UIImage(named: "image1"),UIImage(named: "image2"),UIImage(named: "image3")]
+    
+    //タイマー
+    var timer: Timer!
+    
+    //タイマーの時間のための変数
+    var timer_sec: Int = 0
+    
+    //進むボタンのプロパティ　タップ無効用
+    @IBOutlet weak var nextButton2: UIButton!
+    
+    //戻るボタンのプロパティ　タップ無効用
+    @IBOutlet weak var backButton2: UIButton!
+    
     
     
     override func viewDidLoad() {
@@ -49,7 +60,7 @@ class ViewController: UIViewController {
         self.present(second, animated: true, completion: nil)
     }
     
-    //進む
+    //進むボタン
     @IBAction func nextButton(_ sender: Any) {
         if imageIndex == 2 {
             imageIndex = 0
@@ -57,29 +68,65 @@ class ViewController: UIViewController {
             imageIndex += 1
         }
         slideimage.image = images[imageIndex]
+        
+
+        
     }
     
-    //戻る
+
+    
+    
+    //戻るボタン
     @IBAction func backButton(_ sender: Any) {
+        
         if imageIndex == 0 {
             imageIndex = 2
         } else {
             imageIndex -= 1
         }
         slideimage.image = images[imageIndex]
+        
+        
+        
     }
+    
+
     
     //再生/停止
     @IBAction func startButton(_ sender: Any) {
-     
-     //停止ボタンをタップされるまで繰り返す
-        //もしtimerが2秒だったら
-        //imageIndexに1を足す
-        //imageIndexが2だったら0にする
-        //imageIndexを指定して画像を表示させる
-        //timerを0に戻す
+        
+        //タイマーが存在しない場合だけ、タイマーを作成して動作させる
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+        }
         
         
+        //進むボタン　自動送り中はタップ無効
+        if self.timer != nil {
+            nextButton2.isEnabled = false
+        }
+        
+        //戻るボタン　自動送り中はタップ無効
+        if self.timer != nil {
+            backButton2.isEnabled = false
+        }
+        
+
+        
+    }
+    
+    
+    //タイマー
+    @objc func updateTimer(_ timer: Timer) {
+       
+        if timer_sec % 2 == 0 {    //もしタイマーの秒数が偶数だったら（2で割った余りが0だったら）
+            imageIndex += 1     //imageIndexに1を足す
+            if  imageIndex == 3 {  //もしimageIndexが3になったら
+                imageIndex = 0       //imageIndexを0にする
+            }
+            slideimage.image = images[imageIndex]   //imageIndexを代入して画像を表示させる
+            }
+       
     }
     
 
